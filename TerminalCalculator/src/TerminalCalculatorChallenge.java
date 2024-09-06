028115d6d5f48b1720a8e7d91c2c969181eef572
Gustavo Valença Marcilio <workgustavo10@gmail.com>
2024-09-06 15:05:32 -0300

    Add files via upload

--- TerminalCalculator/src/TerminalCalculatorChallenge.java
+++ TerminalCalculator/src/TerminalCalculatorChallenge.java
@@ -9,25 +9,34 @@ public class TerminalCalculatorChallenge {
         System.out.println("Opção 3: Digite 3");
 
         Scanner scanner = new Scanner(System.in);
-
+        String erro = "";
         int opcoes = scanner.nextInt();
 
-        switch (opcoes) {
-            case 1: {
-                System.out.println("Você selecionou a opção: 1");
+            switch (opcoes) {
+                case 1: {
+                    System.out.println("Você selecionou a opção: 1. E ganhou um sorvete!");
+                    break;
+            }   
+                case 2: {
+                    System.out.println("Você selecionou a opção: 2. E ganhou um sandwich!");
+                    break;
+                }
+                case 3: {
+                    System.out.println("Você selecionou a opção: 3. E ganhou um prato de comida caseiro!");
+                    break;
+                }
+                default:
                 break;
-        }   
-            case 2: {
-                System.out.println("Você selecionou a opção: 2");
-                break;
+            } 
+
+            switch (erro) {
+                case "":
+                System.out.println("Você deve selecionar uma opção disponibilizada no menu.");
+                    break;
+            
+                default:
+                    break;
             }
-            case 3: {
-                System.out.println("Você selecionou a opção: 3");
-                break;
-            }
-            default:
-                break;
-        }
     }
 }
 
